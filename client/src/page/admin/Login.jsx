befa483a0c66e97dea68a8d5293a3d08dfa4200b
billy <billyjoelballola27@gmail.com>
2024-02-25 19:33:23 +0800

    set token

--- client/src/page/admin/Login.jsx
+++ client/src/page/admin/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
       try {
         const { data } = await axios.post('/api/login', loginForm)
         if(data.success) {
-          window.localStorage.setItem('rbim_system', data?.token)
+          window.localStorage.setItem('rbim_system', data?.rbim_token)
           navigate("/rbim/")
           setUpdate("logged-in")
         }else{
